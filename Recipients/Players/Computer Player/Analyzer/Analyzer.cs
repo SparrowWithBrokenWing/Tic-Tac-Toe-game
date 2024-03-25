f42c53af445a06fba15fab5cead8a28c14095b62
SparrowWithBrokenWing <110517253+SparrowWithBrokenWing@users.noreply.github.com>
2024-03-25 21:25:50 +0700

    Finish define abstraction for analyzer, define an unoptimized implement for analyzer. Need to build test.

--- Recipients/Players/Computer Player/Analyzer/Analyzer.cs
+++ Recipients/Players/Computer Player/Analyzer/Analyzer.cs
@@ -1,53 +1,118 @@
 ﻿namespace ComputerPlayer.Analyzer
 {
-    public interface IMoveType { }
-
-    public interface IPossibleMove : IMoveType { }
-    public interface ITacticalMove : IPossibleMove { }
-
-    public interface IOffensiveMove : ITacticalMove { }
-    public interface IForkMove : IOffensiveMove { }
-    public interface IWinningMove : IOffensiveMove { }
-
-    public interface IDefensiveMove : ITacticalMove { }
-    public interface IBlockForkMove : IDefensiveMove { }
-    public interface IBlockWinningMove : IDefensiveMove { }
-
-    public interface ICategorizableMove : IMove
-    {
-        public ICollection<IMoveType> Categories { get; }
-    }
-
-    public interface ICategorizedMove : IMove
-    {
-        public IEnumerable<IMoveType> Categories { get; }
-    }
-
-    public interface IEvaluableMove<TValue> : IMove
-        where TValue : IComparable<TValue>
-    {
-        public TValue EvaluateValue { set; }
-    }
-
-    public interface IEvaluatedMove<TValue> : IMove
-        where TValue : IComparable<TValue>
-    {
-        public TValue EvaluateValue { get; }
-    }
-
     public interface IAnalyzer
     {
         public Tuple<int, int> Analyze();
     }
 
-    public abstract class AbstractAnalyzer<TTreeNode, TNodeValue> : IAnalyzer
-        where TNodeValue : IComparable<TNodeValue>
-        where TTreeNode : ITreeComponent, IEvaluableMove<TNodeValue>, IEvaluatedMove<TNodeValue>, ICategorizableMove, ICategorizedMove
+    public abstract class AbstractAnalyzer<TTreeComponent> : IAnalyzer
+        where TTreeComponent : ITreeComponent, IEvaluableMove, IEvaluatedMove, ICategorizableMove, ICategorizedMove
     {
-        public AbstractAnalyzer(AbstractTree<TTreeNode> tree)
+        public AbstractAnalyzer(
+            IPlayer analyzedPlayer,
+            Tuple<IPlayer, IPlayer> players,
+            IMoveCategorizer moveCategorizer,
+            IMoveRetriever playedMoveRetriever,
+            AbstractTree<TTreeComponent> tree,
+            IMoveEvaluator moveEvaluator)
         {
+            if (!(analyzedPlayer.Equals(players.Item1) || analyzedPlayer.Equals(players.Item2)))
+            {
+                throw new ArgumentException();
+            }
+            AnalyzedPlayer = analyzedPlayer;
+            _players = players;
+            MoveCategrozier = moveCategorizer;
+            PlayedMoveRetriever = playedMoveRetriever;
+            PredictionTree = tree;
+            MoveEvaluator = moveEvaluator;
+        }
 
-            return;
+        protected IPlayer AnalyzedPlayer { get; private set; }
+        private Tuple<IPlayer, IPlayer> _players;
+        protected IPlayer CurrentPlayablePlayer { get => PlayedMoveRetriever.Last().Equals(_players.Item1) ? _players.Item2 : _players.Item1; }
+
+        // for categorizing move
+        protected IMoveCategorizer MoveCategrozier { get; private set; }
+
+        // for calculating move value
+        protected IMoveRetriever PlayedMoveRetriever { get; private set; }
+        protected ITree PredictionTree { get; private set; }
+        protected IMoveEvaluator MoveEvaluator { get; private set; }
+
+        public abstract Tuple<int, int> Analyze();
+    }
+
+    public interface IAnalyzableMove : ITreeComponent, IEvaluableMove, IEvaluatedMove, ICategorizableMove, ICategorizedMove
+    {
+
+    }
+
+    public class UnoptimizedAnalyzer : AbstractAnalyzer<IAnalyzableMove>
+    {
+        public UnoptimizedAnalyzer(
+            IPlayer analyzedPlayer,
+            Tuple<IPlayer, IPlayer> players,
+            IMoveCategorizer moveCategorizer,
+            IMoveRetriever playedMoveRetriever,
+            AbstractTree<IAnalyzableMove> tree,
+            IMoveEvaluator moveEvaluator)
+            : base(analyzedPlayer, players, moveCategorizer, playedMoveRetriever, tree, moveEvaluator)
+        {
+        }
+
+        public override Tuple<int, int> Analyze()
+        {
+            // categorize move type of all predicted moves
+            Action<IMove>? categorize = null;
+            categorize = (IMove move) =>
+            {
+                if (move is ICategorizableMove categorizableMove)
+                {
+                    base.MoveCategrozier.Categorize(categorizableMove);
+                    if (categorizableMove is INextMovesPredictedMove nextMovesPredictedMove)
+                    {
+                        foreach (var predictedNextMove in nextMovesPredictedMove.PredictedNextMoves)
+                        {
+                            if (categorize is not null)
+                            {
+                                categorize(predictedNextMove);
+                            }
+                        }
+                    }
+                }
+            };
+            if (categorize is not null)
+            {
+                categorize(base.PredictionTree.Root);
+            }
+
+            // get the move with best value from all player predicted moves
+            IPlayer analyzedPlayer = AnalyzedPlayer;
+            if (!base.PredictionTree.Root.Player.Equals(analyzedPlayer))
+            {
+                throw new NotImplementedException();
+            }
+            var rowOfBestLocation = -1;
+            var columnOfBestLocation = -1;
+            float maxValue = 0;
+            foreach (var predictedNextMove in base.PredictionTree.Root.PredictedNextMoves)
+            {
+                var valueOfPredictedMove = base.MoveEvaluator.Evaluate(predictedNextMove);
+                if (maxValue < valueOfPredictedMove)
+                {
+                    maxValue = valueOfPredictedMove;
+                    rowOfBestLocation = predictedNextMove.Row;
+                    columnOfBestLocation = predictedNextMove.Column;
+                }
+            }
+
+            if (rowOfBestLocation == -1 || columnOfBestLocation == -1)
+            {
+                throw new Exception();
+            }
+
+            return new Tuple<int, int>(rowOfBestLocation, columnOfBestLocation);
         }
     }
 }
